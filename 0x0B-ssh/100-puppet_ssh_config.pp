161bdda2a9ceeebad97b1f2c4001d2b3f6734eb9
irungundigirigi <irungundigirigi@gmail.com>
2023-11-25 22:49:18 +0300

    ssh tasks add

---
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -0,0 +1,12 @@
+# Puppet script to create ssh config file
+file_line { 'Turn off passwd authentication':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    PasswordAuthentication no',
+}
+
+file_line { 'Declare identity file':
+  ensure => 'present',
+  path   => '/etc/ssh/ssh_config',
+  line   => '    IdentityFile ~/.ssh/school',
+}
