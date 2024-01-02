17cca5b447c1c79c2ccb75983be3336e52f5c67a
irungundigirigi <irungundigirigi@gmail.com>
2024-01-02 22:07:12 +0300

    add task

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,16 +1,26 @@
 #!/usr/bin/python3
-"""Generate a Todo list for a given employee id from REST API"""
+"""
+Write a Python script that, using this REST API, for a given employee ID, returns information about his/her TODO list progress.
+"""
 import requests
-from sys import argv
+import sys
 
 if __name__ == "__main__":
-    url = "https://jsonplaceholder.typicode.com/"
-    id = argv[1]
+    eid = sys.argv[1]
+    name = requests.get("http://jsonplaceholder.typicode.com/users/{}"
+                        .format(eid)).json().get("name")
+    total_tasks = 0
+    done_tasks = []
+    r = requests.get("http://jsonplaceholder.typicode.com/todos").json()
 
-    user = requests.get(url + f"users/{id}").json()
-    todos = requests.get(url + f"todos", params={"userId": id}).json()
+    for task in r:
+        if (task.get("userId") == int(eid)):
+            total_tasks += 1
+            if (task.get("completed")):
+                done_tasks.append(task.get("title"))
 
-    tasks = [i["title"] for i in todos if i["completed"]]
-    print(f"Employee {user.get('name')} is done with ", end="")
-    print(f"tasks({len(tasks)}/{len(todos)}):")
-    [print(f"\t {i}") for i in tasks]
+    print("Employee {} is done with tasks({:d}/{:d}):"
+          .format(name, len(done_tasks), total_tasks))
+
+    for item in done_tasks:
+        print("\t {}".format(item))
