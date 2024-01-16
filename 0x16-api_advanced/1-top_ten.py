d4174b21ecbafbfca680c50e6d3e767ab83ee7f9
irungundigirigi <irungundigirigi@gmail.com>
2024-01-16 17:45:56 +0300

    add reddit hot posts query fn

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,21 @@
+#!/usr/bin/python3
+"""
+Function that queries the Reddit API and prints titles of the first 10 hot
+posts listed for a given subreddit
+"""
 import requests
 
 def top_ten(subreddit):
-    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
-    headers = {'User-Agent': 'MyRedditBot/1.0 (by /u/YourUsername)'}  # Set your own User-Agent
+    """
+    Query Reddit API - if not a valid subreddit, print None
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json"
+    res = requests.get(url, headers={"User-Agent": "Custom"},params={"limit": 10})
 
-    try:
-        response = requests.get(url, headers=headers)
-        data = response.json()
-
-        if response.status_code == 200:
-            posts = data['data']['children'][:10]
-
-            if not posts:
-                print("No posts found.")
-            else:
-                for post in posts:
-                    print(post['data']['title'])
-
-        else:
-            print(f"Error: {data['message']}")
-
-    except requests.exceptions.RequestException as e:
-        print(f"Error: {e}")
+    if res.status_code == 200:
+        for i in res.json().get("data").get("children"):
+            d = i.get("data")
+            title = d.get("title")
+            print(title)
+    else:
+        print(None)
