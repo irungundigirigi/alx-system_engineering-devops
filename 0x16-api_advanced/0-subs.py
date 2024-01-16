dbeb96ef3ca139a726824803646c6bed8cd38671
irungundigirigi <irungundigirigi@gmail.com>
2024-01-16 17:11:33 +0300

    update code

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ Function to query subscribers on a given Reddit Subreddit."""
-
 import requests
 
 def number_of_subscribers(subreddit):
     """ Returns the total number of subscribers on a given subreddit."""
     url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    headers = {"User-Agent": "linux:0x16.api.advanvced:v1.0.0 (by /u/AgitatedPersimmon212)"}
+    headers = {
+            "User-Agent": "linux:0x16.api.advanced:v1.0.0 (by /u/AgitatedPersimmon212)"
+    }
 
     response = requests.get(url, headers=headers, allow_redirects=False)
     if response.status_code == 404:
